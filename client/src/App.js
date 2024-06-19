91f48c4308b82d56f764a6d6a12df09bba962987
NavodyaDhanushka <navodyadhanushka01@gmail.com>
2024-06-19 21:36:59 +0530

    first

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import MainPage from './pages/MainPage';
 import Home from './pages/pamuditha/home'; 
+import SelectServices from './pages/navodya/SelectServices';
+import SelectProfessional from './pages/navodya/SelectProfessional';
+
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/Appoinment" element={<SelectServices />} />
+        <Route path="/Appoinment/professional" element={<SelectProfessional />} />
+        
       </Routes>
     </Router>
   );
